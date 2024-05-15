9855bc07b249672184050ce7d3aaa40e252090a6
Milaadmg023 <132762516+Milaadmg023@users.noreply.github.com>
2024-05-15 09:16:21 +0330

    the end

--- app.js
+++ app.js
@@ -1,40 +1,28 @@
-const sni = document.querySelector(".sni");
-const msg = document.querySelector(".msg");
-const port = document.querySelector(".port");
-const sniCopy = document.querySelector(".sni-copy");
-const msgCopy = document.querySelector(".msg-copy");
-const portCopy = document.querySelector(".port-copy");
-const config = document.querySelector(".old");
-const newConfig = document.querySelector(".new");
+const config = document.querySelector("#config");
+const addBtn = document.querySelector("#add");
+const copyBtn = document.querySelector("#copy");
+const newConfig = document.querySelector("#result");
 
-sniCopy.addEventListener("click", () => {
-  navigator.clipboard.writeText(sni.textContent);
+addBtn.addEventListener("click", () => {
+  newConfig.innerHTML = replaceURL(config.value);
 });
 
-msgCopy.addEventListener("click", () => {
-  navigator.clipboard.writeText(msg.textContent);
-});
-
-portCopy.addEventListener("click", () => {
-  navigator.clipboard.writeText(port.textContent);
+copyBtn.addEventListener("click", () => {
+    navigator.clipboard.writeText(newConfig.innerHTML);
+    copyBtn.innerHTML = "کپی شد";
+    config.value = "";
+    newConfig.innerHTML = "";
 });
 
 function replaceURL(url) {
-    // جایگزینی discord.com:2087 با hello.com
-    url = url.replace(/discord\.com:2087/, "hello.com");
-  
-    // جایگزینی sni=freakshop4444.lmxtinl.com با sni=hello.com
-    url = url.replace(/sni=freakshop4444\.lmxtinl\.com/, "sni=hello.com");
-  
-    return url;
-  }
-  
-  // نمونه استفاده
-  const originalURL = "vless://4357fbb4-2041-48a8-b62a-59942d7db74d@discord.com:2087?security=tls&sni=freakshop4444.lmxtinl.com&alpn=h2&allowInsecure=1&fp=chrome&type=grpc&encryption=none#%F0%9F%87%AC%F0%9F%87%A7%20CDN%20%F0%9F%87%AC%F0%9F%87%A7-122-11%D8%AE%D8%B1%D8%AF%D8%A7%D8%AF";
-  const modifiedURL = replaceURL(originalURL);
-  
-  console.log("URL اصلی:", originalURL);
-  console.log("URL اصلاح شده:", modifiedURL);
+  url = url.replace(/172\.67\.191\.234:2087/, "hello.com");
+  url = url.replace(/zula\.ir:2087/, "hello.com");
+  url = url.replace(/freakshop4444\.lmxtinl\.com:2087/, "hello.com");
+  url = url.replace(/discord\.com:2087/, "hello.com");
 
-  config.innerHTML = originalURL;
-  newConfig.innerHTML = modifiedURL;
+
+  url = url.replace(/sni=freakshop4444\.lmxtinl\.com/, "sni=hello.com");
+
+  return url;
+}
+
